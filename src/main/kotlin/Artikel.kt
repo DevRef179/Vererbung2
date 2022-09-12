5f06948e898152624f90861cdfb94d2f61be6e0b
DevRef <72349761+DevRef179@users.noreply.github.com>
2022-09-12 21:46:50 +0200

    Initial commit

---
+++ src/main/kotlin/Artikel.kt
@@ -0,0 +1,14 @@
+open class Artikel {
+
+    var name: String = ""
+        get() = name
+        set(value) { field = value }
+
+    var preis: Double = 0.0
+        get() = preis
+        set(value) { field = value }
+
+
+
+
+}
---
+++ src/main/kotlin/Warenkorb.kt
@@ -0,0 +1,15 @@
+open class Warenkorb{
+
+
+    fun addArtikel(art: String, preis: Double) {
+
+     val artikel = Artikel()
+        artikel.name = art
+        artikel.preis = preis
+    }
+
+
+}
+
+
+
